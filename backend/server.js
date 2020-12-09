d658eb7ea4b2c4546866d44e868b3a0049068d0d
FaziletKosure <faziletkosure1@gmail.com>
2020-12-09 14:23:34 +0100

    add mongoDB

--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,4 @@ app.get('/api/products/:id', (req,res)=>{
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`))
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))

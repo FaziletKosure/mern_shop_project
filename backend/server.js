e0a613b9c39da6b74f34c374540f3a8b700ef76c
FaziletKosure <faziletkosure1@gmail.com>
2020-12-11 12:59:03 +0100

    added custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const colors =require('colors')
+const errorMiddleware= require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 dotenv.config()
 const productRoutes=require('./routes/productRoutes')
@@ -13,8 +14,10 @@ app.get('/', (req,res)=>{
 // data db
 app.use('/api/products',productRoutes)
 
-
+app.use(errorMiddleware.notFound)
+app.use(errorMiddleware.errorHandler)
 
 const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))

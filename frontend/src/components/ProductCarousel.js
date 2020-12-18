95e4573ebce2bdf1f805db1d3fb0153966b864a1
FaziletKosure <faziletkosure1@gmail.com>
2020-12-18 20:36:03 +0100

    add

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -22,11 +22,11 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant='danger'>{error}</Message>
   ) : (
-    <Carousel pause='hover' className='carousel'>
+    <Carousel pause='hover'  className='carousel'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+            <Image src={product.image} alt={product.name}   fluid />
             <Carousel.Caption className='carousel-caption'>
               <h4>
                 {product.name} (${product.price})

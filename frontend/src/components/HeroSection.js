abcfc60d377c4dc9427731ec03c128a30ed0826b
FaziletKosure <faziletkosure1@gmail.com>
2020-12-19 00:58:54 +0100

    add

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/chanel.mp4' autoPlay loop muted />
+      <video src='/videos/chanel.mp4' autoPlay loop  />
       {/* <h1>BOOST YOUR CAREER</h1> */}
       <p>What are you waiting for?</p>
       <div className='hero-btns'>

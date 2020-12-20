ea61eed3bfd19801b73afa31b4a22ea5c6381bb9
FaziletKosure <faziletkosure1@gmail.com>
2020-12-20 23:30:30 +0100

    add

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from './Button';
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 
 function HeroSection() {
   return (

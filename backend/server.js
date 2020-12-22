7e120c7fe402b61ced1f3b44fe2803b5594009a0
FaziletKosure <faziletkosure1@gmail.com>
2020-12-22 13:17:15 +0100

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
-// const __dirname = path.resolve()
+__dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
